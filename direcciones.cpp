f48283500d1eeebdeba6a6c1be27e22ef849719f
Aida <anoahflinn@gmail.com>
2026-08-27 13:51:32 -0400

    Ejercicio 1a

--- direcciones.cpp
+++ direcciones.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Lab 2 - Pointers" << endl;
-  cout << "HOLAAAAAAAAAAAA" << endl;
+int main()
+ {
+    double precio = 19.99;
+
+    // Dirección de memoria
+    cout << "Direccion de precio: " << &precio << endl;
+
+    // Declarar y asignar un pointer
+    double *pptr = nullptr;
+    pptr = &precio;
+
+    cout << "pptr apunta a: " << pptr << endl;
+    cout << "Valor apuntado (*pptr): " << *pptr << endl;
+
+    // Modificar el valor original a traves del pointer
+    *pptr = 100;
+    cout << "precio despues de *pptr = 100: " << precio << endl;
 
     return 0;
 }
