dc1d35b050d810013747798ab92ac91e5bcb66b7
Aida <anoahflinn@gmail.com>
2026-08-30 02:14:48 -0400

    Ejercicioos de direcciones y pointers basicos.

--- direcciones.cpp
+++ direcciones.cpp
@@ -7,17 +7,23 @@ int main()
 
     // Dirección de memoria
     cout << "Direccion de precio: " << &precio << endl;
+    cout<< "Precio: " << precio << endl;
 
     // Declarar y asignar un pointer
-    double *pptr = nullptr;
-    pptr = &precio;
+    double *ptr = nullptr;
+    ptr = &precio;
 
-    cout << "pptr apunta a: " << pptr << endl;
-    cout << "Valor apuntado (*pptr): " << *pptr << endl;
+    cout << "ptr apunta a: " << ptr << endl;
+    cout << "Valor apuntado (*ptr): " << *ptr << endl;
 
     // Modificar el valor original a traves del pointer
-    *pptr = 100;
-    cout << "precio despues de *pptr = 100:" << precio << endl;
+   // *ptr = 100;
+   // cout << "precio despues de *ptr = 100: " << precio << endl;
 
-    return 0;    
+   //int *malptr=&precio; // no se supone que se supone que puedas poner un double y un int, pero aun asi lo deja
+    //cout<<<"malptr apunta a: " << *malptr << endl;
+
+   // int *sininicializar; pointer q apunta a lo random!
+   // cout << "pointer sin inicializar: " << sininicializar << endl;
+    return 0;
 }
